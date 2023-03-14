3a927202cc3eddefa6e6294cd764f530d1c760ff
Zelirion <sam.poroszlay@gmail.com>
2023-03-14 16:11:10 +0100

    CreateUserCommand fait

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,68 @@
+<?php
+// src/Command/CreateUserCommand.php
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
+// the name of the command is what users type after "php bin/console"
+#[AsCommand(name: 'app:create-user')]
+class CreateUserCommand extends Command
+{
+
+    protected static $defaultName = 'app:create-user';
+    protected static $defaultDescription = 'Creates a new user';
+
+    private EntityManagerInterface $em;
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(EntityManagerInterface $em, UserPasswordHasherInterface $hasher)
+    {
+        $this->em = $em;
+        $this->hasher = $hasher;
+
+        parent::__construct();
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $helper = $this->getHelper('question');
+
+        $questionLogin = new Question('username: ');
+        $questionPassword = new Question('password: ');
+        $questionPassword->setHidden(true);
+        $questionPassword->setHiddenFallback(false);
+
+        $login = $helper->ask($input, $output, $questionLogin);
+        $password = $helper->ask($input, $output, $questionPassword);
+
+        $output->writeln(messages:'Username: '.$login);
+        $output->writeln(messages:'Password: '.$password);
+
+        $user = $this->em->getRepository(User::class)->findAll();
+        if($user){
+            $output->writeln(messages:'User already exists');
+            return Command::FAILURE;
+        }
+
+        $user = new User();
+        $user->setEmail($login);
+        $user->setPassword($this->hasher->hashPassword($user, $password));
+
+        $hash = $this->hasher->hashPassword($user, $user->getPassword());
+        $user->setPassword($hash);
+
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $output->writeln(messages:'User created');
+        return Command::SUCCESS;
+    }
+}
